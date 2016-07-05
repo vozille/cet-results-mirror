6c89c889dd0e2b654381db688daf8c4ce44d6d1e
Anwesh Mohanty <anwesh063@gmail.com>
2016-07-05 10:10:59 +0530

    made more flexible

--- SeleniumScraper.py
+++ SeleniumScraper.py
@@ -9,6 +9,7 @@ from selenium.webdriver.support import expected_conditions as ec
 from selenium.webdriver.support.ui import WebDriverWait
 
 cnt = 0
+
 class Webscraper(threading.Thread):
     def __init__(self, start, end, birthday, url):
         threading.Thread.__init__(self)
@@ -17,7 +18,7 @@ class Webscraper(threading.Thread):
         self.birthday = birthday
         self.url = url
 
-    def find_by_xpath(self, driver, locator, timeout=3):
+    def find_by_xpath(self, driver, locator, timeout=2):
         element = WebDriverWait(driver, timeout).until(
             ec.presence_of_element_located((By.XPATH, locator))
         )
@@ -25,20 +26,39 @@ class Webscraper(threading.Thread):
 
     def run(self):
         global cnt
-        driver = webdriver.Firefox()
+        driver = webdriver.Chrome()
         driver.get(self.url)
         mybirthday = self.birthday.strftime("%d/%m/%Y")
         self.find_by_xpath(driver, '//*[@id="dpStudentdob_dateInput"]').send_keys(mybirthday)
         for roll_num in range(self.start_roll, self.end_roll):
+            result_link = [
+                '//*[@id="gvResultSummary_ctl02_lnkViewResult"]',
+                '//*[@id="gvResultSummary_ctl03_lnkViewResult"]',
+                '//*[@id="gvResultSummary_ctl04_lnkViewResult"]',
+                '//*[@id="gvResultSummary_ctl05_lnkViewResult"]',
+                '//*[@id="gvResultSummary_ctl06_lnkViewResult"]',
+                '//*[@id="gvResultSummary_ctl07_lnkViewResult"]',
+                '//*[@id="gvResultSummary_ctl08_lnkViewResult"]',
+                '//*[@id="gvResultSummary_ctl09_lnkViewResult"]',
+                '//*[@id="gvResultSummary_ctl10_lnkViewResult"]'
+            ]
+
             try:
                 self.find_by_xpath(driver, '//*[@id="txtRegNo"]').clear()
                 self.find_by_xpath(driver, '//*[@id="txtRegNo"]').send_keys(str(roll_num))
                 self.find_by_xpath(driver, '//*[@id="btnView"]').click()
+                # print self.find_by_xpath(driver, '', 1)
                 try:
+                    try:
+                        list_links = driver.find_elements_by_xpath("//*[contains(text(), 'View Result')]")
+                        result_link = list_links[-1].get_attribute('href')[25:60]
+                        result_link = result_link.replace("$", "_")
+                    except:
+                        continue
                     """
                     The webpage has been loaded, do anything you want
                     """
-                    self.find_by_xpath(driver, '//*[@id="gvResultSummary_ctl02_lnkViewResult"]').click()
+                    self.find_by_xpath(driver, '//*[@id="'+result_link+'"]').click()
                     name = self.find_by_xpath(driver, '//*[@id="lblName"]').text
                     branch = self.find_by_xpath(driver, '//*[@id="lblResultName"]').text
                     subjects = self.find_by_xpath(driver, '//*[@id="tblBasicDetail"]/table/tbody/tr[7]/td').text
@@ -52,9 +72,9 @@ class Webscraper(threading.Thread):
                     for k in subjects:
                         print k + ',',
                     print
-                except TimeoutException:
+                except:
                     pass
-            except TimeoutException:
+            except:
                 pass
         driver.close()
 
@@ -63,12 +83,12 @@ def main():
     # dont know why this date works for everyone
     bday = date(1995, 6, 1)
     # start roll
-    i = 1501106100
+    i = 1201106000
     threads = []
-    while i < 1501106105:
-        t = Webscraper(i, i + 1, bday, 'http://www.bputexam.in/StudentSection/ResultPublished/StudentResult.aspx')
+    while i < 1201106400:
+        t = Webscraper(i, i + 80, bday, 'http://www.bputexam.in/StudentSection/ResultPublished/StudentResult.aspx')
         threads.append(t)
-        i += 1
+        i += 80
 
     for i in threads:
         i.start()
@@ -76,7 +96,8 @@ def main():
         time.sleep(6)
     for i in threads:
         i.join()
-    print "Done"
-    
+    print "Done "
+
+
 if __name__ == '__main__':
     main()
